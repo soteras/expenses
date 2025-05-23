4925cd1a2514da659f8e87880b30186eef086feb
Soteras <carlosoteras@gmail.com>
2025-05-23 08:34:19 -0300

    create users migration

---
+++ database/migrations/000002_create_accounts_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE accounts (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name VARCHAR(255) NOT NULL,
+    user_id UUID NOT NULL,
+    created_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP(3) NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
