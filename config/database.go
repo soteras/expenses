71694795f5c5a2894c41bd2189c2ee73a4c86dff
Soteras <carlosoteras@gmail.com>
2025-05-23 07:12:14 -0300

    add database config.

---
+++ config/database.go
@@ -0,0 +1,59 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+type Database struct {
+	Dsn         string
+	DbType      string
+	Debug       bool
+	AutoMigrate bool
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func (d *Database) Connect() (*gorm.DB, error) {
+	var db *gorm.DB
+	var err error
+	config := &gorm.Config{}
+	app := NewAPP()
+
+	if GetEnv("DEBUG") == "true" {
+		newLogger := logger.New(
+			log.New(os.Stdout, "\r\n", log.LstdFlags),
+			logger.Config{
+				SlowThreshold:             time.Second,
+				LogLevel:                  logger.Silent,
+				IgnoreRecordNotFoundError: true,
+				ParameterizedQueries:      true,
+				Colorful:                  false,
+			},
+		)
+
+		config = &gorm.Config{
+			Logger: newLogger,
+		}
+	}
+
+	if app.ENV == EnvTest {
+		db, err = gorm.Open(sqlite.Open("test.db"), config)
+	} else {
+		db, err = gorm.Open(postgres.Open(GetEnv("DB_DSN")), config)
+	}
+
+	if GetEnv("DB_AUTO_MIGRATE") == "true" {
+		db.AutoMigrate()
+	}
+
+	return db, err
+}
---
+++ config/database_test.go
@@ -0,0 +1,25 @@
+package config_test
+
+import (
+	"testing"
+
+	"github.com/soteras/expenses/config"
+	"github.com/stretchr/testify/assert"
+	"gorm.io/gorm"
+)
+
+func TestNewDb(t *testing.T) {
+	db := config.NewDb()
+
+	assert.NotNil(t, db)
+	assert.IsType(t, &config.Database{}, db)
+}
+
+func TestConnec(t *testing.T) {
+	db := config.NewDb()
+
+	conn, err := db.Connect()
+
+	assert.NotNil(t, err)
+	assert.IsType(t, &gorm.DB{}, conn)
+}
